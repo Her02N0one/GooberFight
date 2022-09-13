7efe02a232eafd2c0b9c4685b9175162f97966a6
Ayden Mitchell <38562660+Her02N0one@users.noreply.github.com>
2022-09-13 15:09:28 +0000

    you can punch

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 
-
 @dataclass
 class DefaultStats:
     health: float
@@ -8,14 +7,49 @@ class DefaultStats:
     speed: float
     attack: float
 
+@dataclass
+class PerFightStats: # will be reset to defaults after every fight
+    modifiable_stats: DefaultStats
+    hp: float
+    battle_conditions: int # flags for conditions, probably. unused so far
+    
 
 @dataclass
-class DefultAttack:
+class DefaultAttack:
     name: str
     move: callable
 
+class Fighter:
+    move_limit = 4
+    def __init__(self, name, stats):
+        self.name = name
+        self.stats = stats
+        self.match_stats = PerFightStats(stats, stats.health, 0)
+        self.moves = dict()
+        self.alive = True
 
-@dataclass
-class DefultStyle:
-    name : str
-    moves : list
+    def get_health_percent(self):
+        return self.match_stats.hp/self.stats.health
+    
+    def start_match(self):
+        # reset any stats that were temporarily changed when match starts
+        self.match_stats = PerFightStats(self.stats, self.stats.health, 0)
+        self.alive = True
+
+    def add_move(self, move: DefaultAttack):
+        if (len(self.moves)+1) < self.move_limit:
+            self.moves[move.name] = move
+            
+    
+    def decrease_hp(self, amount):
+        if (self.match_stats.hp - self.match_stats.hp) < 0:
+            self.alive = False
+            return -1
+        self.match_stats.hp -= amount
+
+def simple_damage(attacker: Fighter, opponent: Fighter, damage):
+    opponent.decrease_hp(damage * attacker.stats.attack)
+
+punch = DefaultAttack("punch", (lambda p1, p2: simple_damage(p1, p2, 10)))
+
+
--- main.py
+++ main.py
@@ -1,42 +1,54 @@
 import os
-from data import DefaultStats
+from data import DefaultStats, Fighter, punch
 
+secret_code = "test"
 a = 10
 
 fella = [" o ", "/|\\", "/ \\"]
 
+def progress_bar(percent, length, prefix="HP:", filled="#", empty="_"):
+    ammount_filled = int(length*percent)
+    return f"HP:[{filled*ammount_filled}{empty*(length-ammount_filled)}]"
 
-def print_ui():
-    print("Goober:      HP:[##########]")
+def print_ui(player1, player2):
+    pass
+    print(f"{player2.name}:      {progress_bar(player2.get_health_percent(), 10)}")
     print("\n".join([(" " * 22) + line for line in fella]))
     print("\n".join(fella))
-    print("Shloober:    HP:[##########]")
+    print(f"{player1.name}:      {progress_bar(player1.get_health_percent(), 10)}")
+
+def print_menu():
     print()
     print("| Fight       Counter   |")
     print("| Special     Surrender |")
     print()
-    print("what u tryna do?: ")
 
+def show_moves(player: Fighter):
+    for move in player.moves:
+        print(move)
+    
 
-class Fighter:
-
-    def __init__(self, name, stats: DefaultStats):
-        self.name = name
-        self.stats = stats
-
-
-## since it looks better with an enter between the options and the imput thing, u gotta expand ur consol to see the whole screen
 def main():
-    print_ui()
+    player_1 = Fighter("Goober", DefaultStats(100, 10, 10, 1.1))
+    player_1.add_move(punch)
+    player_2 = Fighter("Shloober", DefaultStats(100, 10, 10, 1.1))
+    player_2.add_move(punch)
+
+    print("Hit enter to start...")
+    
     while True:
         i = input("> ")
         os.system('clear')
-        print_ui()
+        print_ui(player_1, player_2)
+        print("what u tryna do?: ")
 
+        print()
         if i == "quit":
             return 0
-        elif i == "":
-            pass
+        elif i == "fight":
+            show_moves(player_1)
+        elif i in player_1.moves:
+            player_1.moves[i].move(player_1, player_2)
         else:
             print(f'did not understand input: "{i}"')
     return 0
